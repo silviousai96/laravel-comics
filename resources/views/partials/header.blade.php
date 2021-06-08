4c1ff0ff8002348807eb7fa9398f005f013a408b
silvio usai <berzylius@hotmail.it>
2021-06-08 18:07:18 +0200

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,42 @@
 <header>
-    header
+    <div class="top-nav">
+        <div class="wrapper">
+            <span>dc power visa</span>
+            <span>additional dc sites</SPan>
+        </div>
+    </div>
+
+    <div class="bottom-nav">
+        <div class="wrapper">
+            
+            <div class="logo-container">
+                <img src="{{ asset('img/dc-logo.png') }}" alt="">
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li> <a href="#">characters</a></li>
+                    <li> <a href="#">comics</a></li>
+                    <li> <a href="#">movies</a></li>
+                    <li> <a href="#">tv</a></li>
+                    <li> <a href="#">games</a></li>
+                    <li> <a href="#">collectibles</a></li>
+                    <li> <a href="#">videos</a></li>
+                    <li> <a href="#">fans</a></li>
+                    <li> <a href="#">news</a></li>
+                    <li> <a href="#">shop</a></li>
+                </ul>
+            </div>
+
+            <div class="nav-input">
+                <input type="text" placeholder="Search">
+            </div>
+            
+        </div>
+        
+    </div>
+    
+    <div class="jumbotron">
+            
+    </div>
 </header>
